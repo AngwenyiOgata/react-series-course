29d3a952c592f145f95e577a9737a5cb776094b9
Agwenyi Ogata <angwenyiamos01@gnail.com>
2024-02-26 09:37:36 +0300

    understaing react basics, anatomy and basic rules

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,16 @@
+import Youtube from "./Youtube"
+
+function App() {
+
+  return (
+    <>
+      <h1>Basic vite react site</h1>
+      <h1>Basic vite react site</h1>
+
+      <Youtube />
+    </>
+
+  )
+}
+
+export default App
