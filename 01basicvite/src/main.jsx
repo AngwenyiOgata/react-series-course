32a16e7de8c71cd22e4c203bcc0d5731b7032491
Agwenyi Ogata <angwenyiamos01@gnail.com>
2024-02-26 10:33:57 +0300

    custom react

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,6 +1,32 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import React from 'react';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+const reactElem = {
+  type: 'a',
+  props: {
+    href: 'https://google.com',
+    target: '_blank',
+  },
+  children: 'Click to visit google',
+};
+
+// ptoperties should not have login as the logic
+
+function MyApp() {
+  return <div>My basic vite App</div>;
+}
+
+const AnotherElem = (
+  <a href='https://google.com' target='_blank'>
+    Click to go to google{' '}
+  </a>
+);
+
+const actualReactElem = React.createElement(
+  'a',
+  {href: 'https://google.com', target:'_blank'},
+  'click to visit google'
 )
+
+ReactDOM.createRoot(document.getElementById('root')).render( <App /> );
