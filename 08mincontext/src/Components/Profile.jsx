a4b99671e807ce320193665a445a2af17b993bac
Agwenyi Ogata <angwenyiamos01@gnail.com>
2024-02-29 08:37:05 +0300

    UserContext && UserContextProvider

---
+++ 08mincontext/src/Components/Profile.jsx
@@ -0,0 +1,21 @@
+import { useContext } from "react"
+import UserContext from "../context/UserContext"
+
+function Profile() {
+
+  const {user} = useContext(UserContext)
+
+  if (!user) return <h1>Not Loged in</h1>
+
+  return (
+    <div>
+
+    <h1>
+      Profile: {user.username}
+    </h1>
+    <p>More Components</p>
+    </div>
+  )
+}
+
+export default Profile
